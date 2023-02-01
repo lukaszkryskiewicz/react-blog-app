3f245097c0c2e966389353de2e503970253e2bb8
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-02-01 14:51:55 +0100

    Implement application base

---
+++ src/components/views/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar, Container } from "react-bootstrap";
+
+
+const NavBar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" className="mt-4 mb-4 rounded">
+      <Container>
+        <Navbar.Brand href="/">Blog.app</Navbar.Brand>
+        <Nav>
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar;
