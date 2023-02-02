97631553e2b0f0673c4607a658e006a647f66787
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-02-02 15:40:44 +0100

    Create Post component

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,6 +1,52 @@
+import { useParams, Link, Navigate } from "react-router-dom";
+import { getPostById, removePost } from "../../../redux/postsRedux";
+import { useSelector, useDispatch } from "react-redux";
+import { Row, Col, Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+
 const Post = () => {
+  const [show, setShow] = useState(false)
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const postData = useSelector(state => getPostById(state, id));
+
+  const handleClick = () => setShow(true);
+  const handleClose = () => setShow(false)
+
+  const handleDelete = () => {
+    dispatch(removePost(id))
+    setShow(false)
+  }
+
+  if (!postData) return <Navigate to="/" />
+
   return (
-    <h1>Post</h1>
+    <Row className="justify-content-md-center">
+      <Col lg={5}>
+        <h2>{postData.title}</h2>
+        <p><span className="fw-bold">Author:</span> {postData.author}</p>
+        <p><span className="fw-bold">Published:</span> {postData.publishedDate}</p>
+        <p>{postData.content}</p>
+      </Col>
+      <Col lg={3}>
+        <Link to={"/post/edit/" + postData.id}><Button variant="outline-info" className="m-2">Edit</Button></Link>
+        <Button variant="outline-danger" onClick={handleClick}>Delete</Button>
+      </Col>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Lorem Ipsum</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Row>
   )
 }
 
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,11 +1,20 @@
 //selectors
 export const getAllPosts = state => state.posts;
+export const getPostById = ({ posts }, id) => posts.find(post => post.id === id)
+
+
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 // action creators
+export const removePost = payload => ({ type: REMOVE_POST, payload });
+
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter(post => post.id !== action.payload);
     default:
       return statePart;
   };
